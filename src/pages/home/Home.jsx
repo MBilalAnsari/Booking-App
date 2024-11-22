9c27616a346e7819149e45aef492e35a1fd2c79b
Bilal Ansari <m.bilal.ansari.qadri.786@gmail.com>
2024-11-22 21:51:21 +0500

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./home.css"
-import Navbar from '../../components/Navbar/Navbar'
-import Header from '../../components/Header/Header'
-import Featured from '../../components/Featured/Featured'
+import Navbar from '../../components/Navbar/Navbar.jsx'
+import Header from '../../components/Header/Header.jsx'
+import Featured from '../../components/Featured/Featured.jsx'
 
 const Home = () => {
   return (
